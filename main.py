45925c24680bb121e094b645615e38485d675285
Dominik Haeusser <haeusser-dominik@web.de>
2021-12-16 14:13:55 +0100

    improve get_all_users

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
 from configparser import ConfigParser
 from typing import List
 
-import psycopg2
-from pydantic.fields import Field
-from sqlalchemy import create_engine
 from fastapi import FastAPI
 from pydantic import BaseModel
+from pydantic.fields import Field
+from sqlalchemy import text
+from sqlalchemy import engine_from_config
 
 
-def config(filename='database.ini', section='postgresql'):
+def get_config(filename="database.ini", section="test"):
     parser = ConfigParser()
     parser.read(filename)
-
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception(f'Not found section "{section}" in the file "{filename}"')
-
-    return db
+    return dict(parser.items(section))
 
 
-db_inf = config()
-engine = create_engine(f"postgresql+psycopg2://{db_inf['user']}:{db_inf['password']}@{db_inf['host']}/{db_inf['database']}")
-conn = engine.connect()
+engine = engine_from_config(get_config(), prefix='sqlalchemy.')
 app = FastAPI()
 
 
@@ -37,8 +26,20 @@ class User(BaseModel):
     vote_qty: int
 
 
-@app.get("/users/", response_model=List[User], response_model_exclude={"password"})
-def show_all_users():
-    sql = 'SELECT * FROM users ORDER BY user_id;'
-    users = conn.execute(sql).fetchall()
-    return users
+@app.get(
+    "/users/", response_model=List[User], response_model_exclude={"password"}
+)
+async def get_all_users():
+    with engine.connect() as connection:
+        sql = "SELECT * FROM users ORDER BY user_id;"
+        return list(connection.execute(text(sql)))
+
+
+# add change 'accept_invitation status'
+# - a person can change it whenever it want
+
+# add vote_for_songname /vote/{user_id}/
+# - add a function that increase vote_qty from the person who have vote
+# when the person vote valid (valid: not vote the same song twice)
+#  - add song_id and user_id in the same column
+# - if this column exists it is not valid!
