dac7ef8aefa07b3f73ef1f274b30a49720efb929
Dominik Haeusser <haeusser-dominik@web.de>
2021-12-20 12:07:04 +0100

    implement "change invitation status"

--- main.py
+++ main.py
@@ -35,8 +35,15 @@ async def get_all_users():
         return list(connection.execute(text(sql)))
 
 
-# add change 'accept_invitation status'
-# - a person can change it whenever it want
+@app.put("/user/invitation_status/{user_id}", response_model=List[User], response_model_exclude={"password"})
+async def change_invitation_status(user_id: int, answer: bool = None):
+    with engine.connect() as connection:
+        sql = text("UPDATE users SET accept_invitation=:status WHERE user_id=:id;")
+        connection.execute(sql, {"status": answer, "id": user_id})
+        sql2 = text("SELECT * FROM users WHERE user_id=:id;")
+        user = connection.execute(sql2, {"id": user_id})
+        return list(user)
+
 
 # add vote_for_songname /vote/{user_id}/
 # - add a function that increase vote_qty from the person who have vote
